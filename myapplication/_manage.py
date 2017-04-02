515042923db19546ce6cb3e43d7d36fab5fd16aa
bobobobbbb <1105388289@qq.com>
2017-04-02 15:29:37 +0800

    貌似写完了

--- myapplication/_manage.py
+++
@@ -1,9 +0,0 @@
-from myapplication._database import db_session
-
-
-@app.teardown_appcontext                 #As in the declarative approach, you need to close 
-                                         #the session after each request or application context
-                                         # shutdown. Put this into your application module:
-def shutdown_session(exception=None):
-    db_session.remove()
-
